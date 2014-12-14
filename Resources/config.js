1625285e5138c9b27e73cf8a4a4b2ac6cf8d4f0e
motoy3d <motoy3d@gmail.com>
2014-12-15 04:11:47 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'sagan'
+    ,hashtag: 'sagantosu'
+    ,teamName: '鳥栖'
+    ,appName: 'スマートJ for サガン鳥栖'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id926225958?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.sagan'
+    ,color: '#0084c5'
+    ,tintColor: 'e64b8d'
+    ,dbName: 'smartj.sagan'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=sagan&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=sagan"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 251667
+    ,nendApiKeyIPhoneIcon: '5fdd74a107b40e7f12c0c78d7d446c6da233c436'
+    ,nendSpotIdIPhoneBanner: 251666
+    ,nendApiKeyIPhoneBanner: '22ebb33b0c6fcf001107104df66eba1f87554a4e'
+    //nend(Android)
+    ,nendSpotIdAndroid: 251665
+    ,nendApiKeyAndroid: '3e091940304f107ba4d1e805941137fbad0e385c'
+};
